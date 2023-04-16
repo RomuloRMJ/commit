e4fdf2ecbcac97237d8fcb9be6a4a97ff6ee8382
Rômulo Rodrigues <romulo547@hotmail.com>
2023-04-16 13:32:27 -0300

    sdsads
    sdsdsd

--- oi.js
+++ oi.js
@@ -1,8 +1,56 @@
-var a = 1;
-var b = 2;
+// Seleciona o formulário e os campos de entrada
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
 
-if(a > b){
-    console.log("maior")
-}if(b < a){
-    console.log("menor")
-}
+// Adiciona um ouvinte de evento de envio de formulário
+form.addEventListener('submit', function(event) {
+  // Previne o comportamento padrão do envio do formulário
+  event.preventDefault();
+
+  // Verifica se o campo de nome é vazio
+  if (nameInput.value === '') {
+    // Exibe mensagem de erro para o campo de nome
+    const nameError = document.querySelector('#name-error');
+    nameError.innerHTML = 'Por favor, informe seu nome.';
+    nameError.classList.add('error-message');
+  } else {
+    // Remove mensagem de erro do campo de nome
+    const nameError = document.querySelector('#name-error');
+    nameError.innerHTML = '';
+    nameError.classList.remove('error-message');
+  }
+
+  // Verifica se o campo de email é válido
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailInput.value)) {
+    // Exibe mensagem de erro para o campo de email
+    const emailError = document.querySelector('#email-error');
+    emailError.innerHTML = 'Por favor, informe um email válido.';
+    emailError.classList.add('error-message');
+  } else {
+    // Remove mensagem de erro do campo de email
+    const emailError = document.querySelector('#email-error');
+    emailError.innerHTML = '';
+    emailError.classList.remove('error-message');
+  }
+
+  // Verifica se o campo de senha tem pelo menos 8 caracteres
+  if (passwordInput.value.length < 8) {
+    // Exibe mensagem de erro para o campo de senha
+    const passwordError = document.querySelector('#password-error');
+    passwordError.innerHTML = 'A senha deve ter pelo menos 8 caracteres.';
+    passwordError.classList.add('error-message');
+  } else {
+    // Remove mensagem de erro do campo de senha
+    const passwordError = document.querySelector('#password-error');
+    passwordError.innerHTML = '';
+    passwordError.classList.remove('error-message');
+  }
+
+  // Se todos os campos são válidos, envia o formulário
+  if (nameInput.value !== '' && emailRegex.test(emailInput.value) && passwordInput.value.length >= 8) {
+    form.submit();
+  }
+});
