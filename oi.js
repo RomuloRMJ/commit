4c5107beb2c48d9203e498637924c8d38b228730
Rômulo Rodrigues <romulo547@hotmail.com>
2023-04-16 13:29:22 -0300

    asds
    sds

--- oi.js
+++ oi.js
@@ -1,11 +1,8 @@
 var a = 1;
 var b = 2;
-var c = 3;
 
 if(a > b){
-    return "maior"
-} else if(b > c){
-    return "maior"
-}else{
-    return "menor"
+    console.log("maior")
+}if(b < a){
+    console.log("menor")
 }
